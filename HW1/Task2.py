d0ff372b78949304e95826a5346f7296d654d331
Ekaterina2207 <m2510519@edu.misis.ru>
2025-09-17 23:01:02 +0300

    дз(1 и 2 упр)

---
+++ HW1/Task2.py
@@ -0,0 +1,40 @@
+from math import *
+
+a = int(input())
+b = int(input())
+info = '''
+a + b = {plus}
+a - b = {minus}
+a * b = {mult}
+a / b = {delit}
+a // b = {celoe}
+a % b = {ostatok}
+a ** b = {stepen}
+log10(a) = {lga} 
+log10(b) = {lgb}
+a < b = {smen}
+a <= b = {men}
+a > b = {sbol}
+a >= b = {bol}
+a != b = {neravno}
+a == b = {ravno}
+'''
+print(
+    info.format(
+        plus=a + b,
+        minus=a - b,
+        mult=a * b,
+        delit=round(a / b, 2),
+        celoe=a // b,
+        ostatok=a % b,
+        stepen=a ** b,
+        lga=round(log10(a), 2),
+        lgb=round(log10(b), 2),
+        smen=a < b,
+        men=a <= b,
+        sbol=a > b,
+        bol=a >= b,
+        neravno=a != b,
+        ravno=a == b
+    )
+)
